4cb84b7f7f04179481c8fe5b5f0cb6c29fcc0fc7
grace butler <gmx.butler@gmail.com>
2021-07-16 14:56:19 -0500

    created server.js file. used example project as guide. Added routes folder and js files. added packages

---
+++ server.js
@@ -0,0 +1,21 @@
+//dependencies
+const express = require("express");
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+//inital port
+const PORT = process.env.PORT || 3000;
+
+//Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//router
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+//"starts" server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
