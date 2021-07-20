48ade1f48dbd4718e98b8d6ba2c2308079a4a426
grace butler <gmx.butler@gmail.com>
2021-07-19 20:47:48 -0500

    trying to fix 'ENOENT: no such file or directory, open '/db/db.json'' error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 // LOAD DATA
 // We are linking our routes to a series of "data" sources. These data sources hold arrays of information 
 const fs = require ('fs')
-var dbData = JSON.parse(fs.readFileSync('../db/db.json','utf8'));
+var dbData = JSON.parse(fs.readFileSync('/db/db.json','utf8'));
 // ROUTING
 module.exports = (app) => {
 
@@ -22,7 +22,7 @@ app.get('/api/notes/:id', function(req, res){
     newNote.id= idNum;
     data.push(newNote);
   
-    fs.writeFileSync('../db/db.json', JSON.stringify(dbData), function(err) {
+    fs.writeFileSync('/db/db.json', JSON.stringify(dbData), function(err) {
       if (err) throw (err);
     });
     
