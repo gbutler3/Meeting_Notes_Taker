73600e4f2f4d58a5036980e0d2de654a4dedcd61
grace butler <gmx.butler@gmail.com>
2021-07-22 14:03:24 -0500

    static assets added

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"))
+
 //router
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
